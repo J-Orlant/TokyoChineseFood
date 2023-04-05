1d444a6563c9ffaca32e086b4a6fd2d1365eadd4
EvanAudi <127534142+EvanAudi@users.noreply.github.com>
2023-04-05 21:07:06 +0700

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Guys!");
+        System.out.println("Hello, World!");
+        System.out.println("Bapakmu Bapakku BapakKita");
     }
 }
